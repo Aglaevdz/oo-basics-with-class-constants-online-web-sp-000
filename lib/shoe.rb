1d7afe1220c3845e4f97b752c0962e319aece245
Aglae Verduzco <aglae.cv@gmail.com>
2019-01-05 22:48:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if brand.any? |brand| BRANDS << @brand
+    if brand.any? do |brand| BRANDS << @brand
     #if !(BRANDS.include?(@brand))
         #BRANDS << @brand
   end
