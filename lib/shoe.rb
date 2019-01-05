ee85f446161edc1116d5cf9492f1f85df4fca134
Aglae Verduzco <aglae.cv@gmail.com>
2019-01-05 21:48:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 
 class Shoe
-  attr_accessor :color, :size, :material, :condition, :brand
-  attr_reader
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
