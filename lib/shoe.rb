a080c483b3b1cbbff8a72121e7ec12ea69212583
Aglae Verduzco <aglae.cv@gmail.com>
2019-01-05 22:01:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,16 +10,8 @@ class Shoe
     BRANDS << brand
   end
 
-  def brand=(brand)
-    @brand
-    BRANDS << brand
-  end
-
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-
-
 end
