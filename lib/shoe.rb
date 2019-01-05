ee094d186154a78f71889e87658980906c3e71ce
Aglae Verduzco <aglae.cv@gmail.com>
2019-01-05 21:45:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition, :brand
   attr_reader
 
-  BRANDS = []
+
 
   def initialize(brand)
     @brand = brand
   end
 
+  BRANDS = []
   def cobble
     BRANDS << brand
     self.condition = "new"
